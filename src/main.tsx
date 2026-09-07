7732bb1567f3b04c5e813ed735f872b0d5dfc251
zagzy8776 <127265311+zagzy8776@users.noreply.github.com>
2026-09-07 14:16:00 -0400

    activate product SEO synchronization on product routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./app/App.tsx";
 import AdminApp from "./app/admin/AdminApp.tsx";
 import OrderTrackPage from "./app/OrderTrackPage.tsx";
+import ProductSeoSync from "./app/ProductSeoSync.tsx";
 import "./styles/index.css";
+
+function ProductRoute() {
+  return (
+    <>
+      <ProductSeoSync />
+      <App />
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/admin/*" element={<AdminApp />} />
       <Route path="/order/:orderId" element={<OrderTrackPage />} />
-      <Route path="/product/:productId" element={<App />} />
+      <Route path="/product/:productId" element={<ProductRoute />} />
       <Route path="/*" element={<App />} />
     </Routes>
   </BrowserRouter>
